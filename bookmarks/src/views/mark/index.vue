1e071d19fe7a71db640cb5146791309a34299dc5
RunoobDeve <1757493893@qq.com>
2022-04-18 17:59:55 +0800

    fix bug

--- bookmarks/src/views/mark/index.vue
+++ bookmarks/src/views/mark/index.vue
@@ -28,66 +28,68 @@
         </div>
         <a id="mark0" class="classify-title">我的</a>
         <div class="mark-box">
-          <a
-            :href="item.url"
-            target="_blank"
+          <div
+            class="mark-item"
             v-for="(item, index) in myCollect"
             :key="index"
-            class="mark-item"
           >
-            <div class="mark-logo">
-              <!-- <img :src="'/static/img/' + item.image" alt="" /> -->
-              <img :src="item.url | webDomain" alt="" />
+            <a :href="item.url" target="_blank" class="mark-wrapper">
+              <div class="mark-logo">
+                <!-- <img :src="'/static/img/' + item.image" alt="" /> -->
+                <img :src="item.url | webDomain" alt="" />
+              </div>
+              <div>
+                <a :href="item.url" target="_blank" class="mark-name">{{
+                  item.name
+                }}</a>
+                <div class="mark-desc">{{ item.description }}</div>
+              </div>
+              <div @click.prevent="delCollect(index)" class="mark-check">
+                <i class="iconfont icondel"></i>
+              </div>
+            </a>
+          </div>
+          <div class="mark-item">
+            <div class="mark-add" @click="showAddForm">
+              <i class="iconfont iconjiahao"></i>
             </div>
-            <div>
-              <a :href="item.url" target="_blank" class="mark-name">{{
-                item.name
-              }}</a>
-              <div class="mark-desc">{{ item.description }}</div>
-            </div>
-            <div @click.prevent="delCollect(index)" class="mark-check">
-              <i class="iconfont icondel"></i>
-            </div>
-          </a>
-          <div class="mark-add" @click="showAddForm">
-            <i class="iconfont iconjiahao"></i>
           </div>
         </div>
       </div>
       <div v-for="(item, index) in resource" :key="index" class="classify-box">
         <a :id="'mark' + (index + 1)" class="classify-title">{{ item.name }}</a>
         <div class="mark-box">
-          <a
-            :href="mark.url"
-            target="_blank"
+          <div
+            class="mark-item"
             v-for="(mark, markIndex) in item.site"
             :key="markIndex"
-            class="mark-item"
           >
-            <div class="mark-logo">
-              <img :src="'/static/img/' + mark.image" alt="" />
-            </div>
-            <div>
-              <a :href="mark.url" target="_blank" class="mark-name">{{
-                mark.name
-              }}</a>
-              <div class="mark-desc">{{ mark.description }}</div>
-            </div>
-            <div
-              v-if="collectIds.indexOf(mark.id) == -1"
-              @click.prevent="addCollect(index, markIndex, mark.id)"
-              class="mark-check"
-            >
-              <i class="iconfont iconjiahao"></i>
-            </div>
-            <div
-              v-else
-              @click.prevent="addCollect(index, markIndex, mark.id)"
-              class="mark-check"
-            >
-              <i class="iconfont icondel"></i>
-            </div>
-          </a>
+            <a :href="mark.url" target="_blank" class="mark-wrapper">
+              <div class="mark-logo">
+                <img :src="'/static/img/' + mark.image" alt="" />
+              </div>
+              <div>
+                <a :href="mark.url" target="_blank" class="mark-name">{{
+                  mark.name
+                }}</a>
+                <div class="mark-desc">{{ mark.description }}</div>
+              </div>
+              <div
+                v-if="collectIds.indexOf(mark.id) == -1"
+                @click.prevent="addCollect(index, markIndex, mark.id)"
+                class="mark-check"
+              >
+                <i class="iconfont iconjiahao"></i>
+              </div>
+              <div
+                v-else
+                @click.prevent="addCollect(index, markIndex, mark.id)"
+                class="mark-check"
+              >
+                <i class="iconfont icondel"></i>
+              </div>
+            </a>
+          </div>
         </div>
       </div>
       <div class="web-desc"></div>
@@ -174,6 +176,13 @@ export default {
       localStorage.setItem("myCollect", JSON.stringify(this.myCollect));
     },
     exportJson() {
+      if (this.myCollect.length == 0) {
+        this.$message({
+          type: "warning",
+          message: "我的收藏为空",
+        });
+        return false;
+      }
       var data = JSON.stringify(this.myCollect);
       let uri = "data:text/json;charset=utf-8," + encodeURIComponent(data);
       let link = document.createElement("a");
@@ -183,6 +192,10 @@ export default {
       document.body.appendChild(link);
       link.click();
       document.body.removeChild(link);
+      this.$message({
+        type: "success",
+        message: "导出成功",
+      });
     },
     importJson() {
       this.$nextTick(() => {
@@ -250,6 +263,10 @@ export default {
         const myCollect = JSON.parse(reader.result);
         this.myCollect = [...this.myCollect, ...myCollect];
         localStorage.setItem("myCollect", JSON.stringify(this.myCollect));
+        this.$message({
+          type: "success",
+          message: "导入成功",
+        });
       };
     });
   },
@@ -297,65 +314,70 @@ export default {
     display: block;
   }
   .mark-box {
-    display: grid;
-    // align-items: center;
-    // flex-wrap: wrap;
-    grid-template-columns: repeat(5, 1fr);
-    grid-row-gap: 15px;
-    grid-column-gap: 15px;
+    display: flex;
+    align-items: center;
+    flex-wrap: wrap;
+    // display: grid;
+    // grid-template-columns: repeat(5, 1fr);
+    // grid-row-gap: 15px;
+    // grid-column-gap: 15px;
     .mark-item {
-      background-color: #fff;
-      border-radius: 5px;
-      padding: 10px;
-      box-shadow: 0px 0px 10px -5px rgba(158, 158, 158, 0.1);
-      display: flex;
-      justify-content: flex-start;
-      align-items: flex-start;
-      height: 90px;
-      position: relative;
-      transition: all 0.3s;
-      &:hover {
-        box-shadow: 0px 0px 20px -5px rgba(158, 158, 158, 0.2);
-        transform: translateY(-6px);
-        .mark-check {
-          opacity: 1;
-        }
-      }
-      .mark-logo {
-        width: 50px;
-        height: 50px;
-        border-radius: 50%;
-        margin-right: 10px;
-        flex-shrink: 0;
-        padding: 5px;
-        img {
-          width: 100%;
-        }
-      }
-      .mark-name {
-        font-weight: bold;
-        color: #000;
-      }
-      .mark-desc {
-        color: #a0aec0;
-        font-size: 14px;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        display: -webkit-box;
-        -webkit-line-clamp: 2;
-        -webkit-box-orient: vertical;
-        line-height: 22px;
-        margin-top: 5px;
-      }
-      .mark-check {
-        position: absolute;
-        right: 10px;
-        top: 10px;
-        opacity: 0;
+      padding: 5px 10px;
+      width: 20%;
+      .mark-wrapper {
+        background-color: #fff;
+        border-radius: 5px;
+        padding: 10px;
+        box-shadow: 0px 0px 10px -5px rgba(158, 158, 158, 0.1);
+        display: flex;
+        justify-content: flex-start;
+        align-items: flex-start;
+        height: 90px;
+        position: relative;
         transition: all 0.3s;
-        i {
-          color: #2ed573;
-          font-size: 18px;
+        &:hover {
+          box-shadow: 0px 0px 20px -5px rgba(158, 158, 158, 0.2);
+          transform: translateY(-6px);
+          .mark-check {
+            opacity: 1;
+          }
+        }
+        .mark-logo {
+          width: 50px;
+          height: 50px;
+          border-radius: 50%;
+          margin-right: 10px;
+          flex-shrink: 0;
+          padding: 5px;
+          img {
+            width: 100%;
+          }
+        }
+        .mark-name {
+          font-weight: bold;
+          color: #000;
+        }
+        .mark-desc {
+          color: #a0aec0;
+          font-size: 14px;
+          overflow: hidden;
+          text-overflow: ellipsis;
+          display: -webkit-box;
+          -webkit-line-clamp: 2;
+          -webkit-box-orient: vertical;
+          line-height: 22px;
+          margin-top: 5px;
+        }
+        .mark-check {
+          position: absolute;
+          right: 10px;
+          top: 10px;
+          opacity: 0;
+          transition: all 0.3s;
+          i {
+            color: #2ed573;
+            font-size: 18px;
+          }
         }
       }
     }
@@ -569,6 +591,16 @@ export default {
   }
   &.show-form {
     transform: translateX(0);
+  }
+}
+@media screen and (max-width: 1440px) {
+  .classify-box .mark-box .mark-item {
+    width: 25%;
+  }
+}
+@media screen and (max-width: 1280px) {
+  .classify-box .mark-box .mark-item {
+    width: 33%;
   }
 }
 </style>
